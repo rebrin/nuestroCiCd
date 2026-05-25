a50b2f58998cb786fb5a8684f7f8eda118d5741b
rebrin-lap <rebrin53@gmail.com>
2026-05-25 09:57:31 -0600

    Rebrindanard:siguiente commit

--- index.test.js
+++ index.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("./index");
 
 describe("GET /", () => {
-  it('should return "mi primer CI/CD"', async () => {
+  it('should return "mi primer CI/CD y nuestros cambios tambien"', async () => {
     const res = await request(app).get("/");
     expect(res.statusCode).toBe(200);
     expect(res.body.mensaje).toBe("mi primer CI/CD");
